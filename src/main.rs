b4d6e0030a3eb343ce6d07e135d72aa57982a943
Oneirical <manchot@videotron.ca>
2023-10-31 17:00:14 -0400

    feat: suffering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
     let map = Map::new_map();
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Update, hello_world)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, draw_creature)
         .run();
 }
 
@@ -26,6 +28,32 @@ pub struct Creature {
 #[derive(Component, Debug, PartialEq, Copy, Clone)]
 pub struct Floor {
     pub creature_id : i32,
+}
+
+pub fn draw_creature(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("22677410.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
 
 impl Map {

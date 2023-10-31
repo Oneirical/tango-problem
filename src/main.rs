39d4d4ac73dfe9ab1c0e2e9206d7a6ee026109cf
Oneirical <manchot@videotron.ca>
2023-10-31 19:01:13 -0400

    feat: grid

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
 
 fn main() {
-    let map = Map::new_map();
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, draw_creature)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "TGFP".into(),
+                        resolution: (640.0, 480.0).into(),
+                        //resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .add_systems(Startup, create_map)
+        .add_systems(Update, character_movement)
         .run();
-}
-
-fn hello_world() {
-
 }
 
 pub struct Map {
@@ -28,32 +35,53 @@ pub struct Creature {
 #[derive(Component, Debug, PartialEq, Copy, Clone)]
 pub struct Floor {
     pub creature_id : i32,
+    pub x: i32,
+    pub y: i32,
 }
 
-pub fn draw_creature(
+pub fn create_map(
     mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
 ) {
-    let window = window_query.get_single().unwrap();
+    commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-            texture: asset_server.load("22677410.png"),
+    let play_space = Map::new_map();
+    for i in play_space.tiles{
+        let texture = asset_server.load("22677410.png");
+        commands.spawn(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(64.0, 64.0)),
+                ..default()
+            },
+            texture,
+            transform: Transform {
+                translation: Vec3{ x: i.x as f32 * 64.0, y: i.y as f32 * 64.0, z: 0.0},
+                ..default()
+            },
             ..default()
-        },
-        Player {},
-    ));
+        });
+    }
 }
 
-pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window = window_query.get_single().unwrap();
-
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-        ..default()
-    });
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 1000.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 1000.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 1000.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 1000.0 * time.delta_seconds();
+        }
+    }
 }
 
 impl Map {
@@ -62,13 +90,26 @@ impl Map {
     }
     fn new_map() -> Map {
         let empty_floor = Floor{
-            creature_id : -1, 
+            creature_id : -1,
+            x: 0,
+            y: 0,
         };
-        let map = Map{
+        let mut map = Map{
             tiles: vec![empty_floor; 45*45],
             width : 45,
             height : 45,
         };
+        let mut x_count = 0;
+        let mut y_count = 0;
+        for i in &mut map.tiles{
+            if x_count > map.width{
+                x_count = 0;
+                y_count += 1;
+            }
+            i.x = x_count;
+            i.y = y_count;
+            x_count += 1;
+        }
         map
     }
 }
